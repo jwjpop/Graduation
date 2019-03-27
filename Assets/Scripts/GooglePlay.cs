5c30a746eeb305919aa69b9d3faf87c139e891f0
woojin <jwjppp@naver.com>
2019-03-27 17:35:42 +0900

    구글플레이 연동중

---
+++ Assets/Scripts/GooglePlay.cs
@@ -0,0 +1,118 @@
+﻿using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooglePlay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            .EnableSavedGames()
+            .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+
+        PlayGamesPlatform.DebugLogEnabled = true;
+
+        PlayGamesPlatform.Activate();
+
+        
+    }
+
+    public void SignIn()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                Debug.Log("success");
+                // to do ...
+                // 로그인 성공 처리
+            }
+            else
+            {
+                Debug.Log("fail");
+                // to do ...
+                // 로그인 실패 처리
+            }
+        });
+    }
+    public void UnlockAchievement(int score)
+    {
+        if (score >= 100)
+        {
+
+            PlayGamesPlatform.Instance.ReportProgress(GPGSIds.achievement_level1, 100f, null);
+
+        }
+    }
+    public void ShowAchievementUI()
+    {
+        // Sign In 이 되어있지 않은 상태라면
+        // Sign In 후 업적 UI 표시 요청할 것
+        if (Social.localUser.authenticated == false)
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                if (success)
+                {
+                    // Sign In 성공
+                    // 바로 업적 UI 표시 요청
+                    Social.ShowAchievementsUI();
+                    return;
+                }
+                else
+                {
+                    // Sign In 실패 처리
+                    return;
+                }
+            });
+        }
+
+        Social.ShowAchievementsUI();
+    }
+    public void ReportScore(int score)
+    {
+        PlayGamesPlatform.Instance.ReportScore(score, GPGSIds.leaderboard_score, (bool success) =>
+        {
+            if (success)
+            {
+                // Report 성공
+                // 그에 따른 처리
+            }
+            else
+            {
+                // Report 실패
+                // 그에 따른 처리
+            }
+        });
+    }
+    public void ShowLeaderboardUI()
+    {
+        // Sign In 이 되어있지 않은 상태라면
+        // Sign In 후 리더보드 UI 표시 요청할 것
+        if (Social.localUser.authenticated == false)
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                if (success)
+                {
+                    // Sign In 성공
+                    // 바로 리더보드 UI 표시 요청
+                    Social.ShowLeaderboardUI();
+                    return;
+                }
+                else
+                {
+                    // Sign In 실패 
+                    // 그에 따른 처리
+                    return;
+                }
+            });
+        }
+        PlayGamesPlatform.Instance.ShowLeaderboardUI();
+    }
+}
