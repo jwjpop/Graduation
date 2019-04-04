6792833f194b2a7f924c0a05f6b352072a3dbc18
jwjpop <jwjppp@naver.com>
2019-04-04 17:44:57 +0900

    테스트용 리셋버튼 추가

--- Assets/Scripts/DataControl.cs
+++ Assets/Scripts/DataControl.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class DataControl : MonoBehaviour
 {
+    //돈 관리
     public static int CurMoney = 0 ;
     public Text CurText;
     // Start is called before the first frame update
     void Start()
     {
+        //UnUseBlockMain에서 0으로 초기화
+        //처음 켜졌으면
         if (UnUseBlockMain.stageTitleIndex == 0)
             UnUseBlockMain.stageTitleIndex = PlayerPrefs.GetInt("stage", 1);
         //1~8탄 테스트용
@@ -19,6 +22,21 @@ public class DataControl : MonoBehaviour
         Debug.Log(UnUseBlockMain.stageTitleIndex);
         CurMoney = PlayerPrefs.GetInt("money",300000);
         CurText.GetComponent<Text>().text = CurMoney.ToString();
+    }
+
+    public void moneyReset()
+    {
+        CurMoney = 0;
+        PlayerPrefs.SetInt("money", CurMoney);
+        PlayerPrefs.Save();
+        CurText.GetComponent<Text>().text = CurMoney.ToString();
+    }
+
+    public void stageReset()
+    {
+        UnUseBlockMain.stageTitleIndex = 1;
+        PlayerPrefs.SetInt("stage", UnUseBlockMain.stageTitleIndex);
+        PlayerPrefs.Save();
     }
     
 }
